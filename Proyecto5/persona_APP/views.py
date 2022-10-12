318c9036d1f6589e90b1314bbc4fd1c002d9f247
orionpgaete <pedro.gaete@pgcomputer.cl>
2022-10-12 15:23:08 -0300

    mostrar datos

--- Proyecto5/persona_APP/views.py
+++ Proyecto5/persona_APP/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from persona_APP.models import persona
 # Create your views here.
+
+def personadata(request):
+    personas = persona.objects.all()
+    data = {'personas' : personas}
+    return render(request, 'empleados.html', data)
